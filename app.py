a5da7cfbec3ad48ff3347b222bd0baab8e14654a
rokith-dev <rokith466@gmail.com>
2026-06-06 15:09:42 +0530

    adding multiple languages in model

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ from src.model import translate_text
 
 st.title("🌍 Language Translator AI")
 
+language = st.selectbox(
+    "Select Language",
+    ["Tamil", "Hindi", "French", "German"]
+)
+
 text = st.text_area("Enter English Text")
 
 if st.button("Translate"):
-    result = translate_text(text)
+    result = translate_text(text, language)
 
-    st.subheader("Tamil Translation")
+    st.subheader(f"{language} Translation")
     st.write(result)
--- src/model.py
+++ src/model.py
@@ -1,9 +1,16 @@
 from deep_translator import GoogleTranslator
 
-def translate_text(text):
+LANGUAGE_CODES = {
+    "Tamil": "ta",
+    "Hindi": "hi",
+    "French": "fr",
+    "German": "de"
+}
+
+def translate_text(text, language):
     translated = GoogleTranslator(
         source="en",
-        target="ta"
+        target=LANGUAGE_CODES[language]
     ).translate(text)
 
     return translated
