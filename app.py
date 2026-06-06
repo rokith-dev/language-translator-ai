2ac2384020d88800ae737301eb3550740e78cff8
rokith-dev <rokith466@gmail.com>
2026-06-06 15:25:27 +0530

    checking the database with streamlit done successfully

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from src.model import translate_text
+from src.database import save_translation
 
 st.title("🌍 Language Translator AI")
 
@@ -11,7 +12,14 @@ language = st.selectbox(
 text = st.text_area("Enter English Text")
 
 if st.button("Translate"):
+
     result = translate_text(text, language)
+
+    save_translation(
+        text,
+        language,
+        result
+    )
 
     st.subheader(f"{language} Translation")
     st.write(result)
--- src/database.py
+++ src/database.py
@@ -7,3 +7,23 @@ def get_connection():
         password="root123",
         database="translator_db"
     )
+
+
+def save_translation(input_text, language, translated_text):
+    conn = get_connection()
+
+    cursor = conn.cursor()
+
+    query = """
+    INSERT INTO translations
+    (input_text, language, translated_text)
+    VALUES (%s, %s, %s)
+    """
+
+    cursor.execute(
+        query,
+        (input_text, language, translated_text)
+    )
+
+    conn.commit()
+    conn.close()
